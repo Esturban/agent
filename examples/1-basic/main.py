4f8481e0c8339f0aad76defbc1afd7f36481b403
Esturban <info@evadvisory.ca>
2025-09-21 20:08:28 +0300

    feat: basic rag agent with chroma and how to split documents
    a simple local implementation of a RAG model to serve a a POC

--- examples/1-basic/main.py
+++ examples/1-basic/main.py
@@ -1,43 +1,14 @@
 from typing import Literal
 from langchain_core.messages import HumanMessage
 from langchain_openai.chat_models import ChatOpenAI
-from langchain_community.document_loaders import UnstructuredURLLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.vectorstores import Chroma
-from langchain_openai.embeddings import OpenAIEmbeddings
 from langchain_core.tools import tool
 from langgraph.checkpoint.memory import MemorySaver
 from langgraph.graph import END, START, StateGraph, MessagesState
 from langgraph.prebuilt import ToolNode
-import os
-
 from dotenv import load_dotenv
-load_dotenv()
+from src.utils import retrieve_context
 
-@tool
-def retrieve_context(query: str):
-    """Search for relevant documents."""
-    urls = [
-        "https://docs.python.org/3/tutorial/index.html",
-        "https://realpython.com/python-basics/",
-        "https://www.learnpython.org/"
-    ]
-    #Loading all of the URLs
-    loader = UnstructuredURLLoader(urls=urls)
-    docs = loader.load()
-    #Splitting the documents into chunks
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=100, chunk_overlap=50)
-    doc_splits = text_splitter.split_documents(docs)
-    
-    # Creating the vectorstore with chroma
-    vectorstore = Chroma.from_documents(
-        documents=doc_splits,
-        collection_name="python_docs",
-        embedding=OpenAIEmbeddings(),
-    )
-    retriever = vectorstore.as_retriever()
-    results = retriever.invoke(query)
-    return "\n".join([doc.page_content for doc in results])
+load_dotenv()
 
 #Creating the tool node
 tools = [retrieve_context]
