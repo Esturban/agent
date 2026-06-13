4ce94aa567e9261c860a0888a185f28763267d06
Esturban <info@evadvisory.ca>
2026-06-13 08:52:15 -0400

    feat(examples/81-streaming-sse-server): add tools -- DEMO_QUESTION and create_langchain_app factory

--- examples/81-streaming-sse-server/src/tools.py
+++ examples/81-streaming-sse-server/src/tools.py
@@ -1,1 +1,7 @@
-pass
+from src.workflow import create_simple_graph
+
+DEMO_QUESTION = "Explain how photosynthesis works in 2 sentences."
+
+
+def create_langchain_app():
+    return create_simple_graph()
